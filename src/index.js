2d2fac87ee6fadc23276ba8ef759eff63196f2cc
randy brown <thatrandybrown@gmail.com>
2020-05-12 07:26:09 -0500

    capture the CID and an entry stub
    this is primarily for displaying the list of entries and allowing a user to
    return to previous entries for editing and updating.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-
 (document.onLoad = () => {
     let change = 0;
-    let metadata = {};
 
     let records = JSON.parse(window.localStorage.getItem('records')) || {};
     let cid = Object.keys(records).length;
+
+    const positiveInstance = num => num < 0 ? 0 : num;
+    const firstLine = str => str.substr(0, Math.max(positiveInstance(str.indexOf("\n")), positiveInstance(str.indexOf(".") + 1)) || 40);
+
+    const entries = Object.keys(records).reduce((acc, item) => [...acc, {...records[item][records[item].length - 1], stub: firstLine(records[item][records[item].length - 1].content)}], []).map(({cid, stub}) => ({cid, stub}));
 
     const saveData = async () => {
         records = {
