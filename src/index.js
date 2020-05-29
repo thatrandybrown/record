2d5d517edd921f734f82b0f9202b9b885c6203cc
randy brown <thatrandybrown@gmail.com>
2020-05-28 22:01:12 -0500

    extract the records and app metadata from the local storage
    this may migrate to a cache or something else, but for now localstorage will
    store the app data and the app data data.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@
     const textarea = document.querySelector('textarea');
     const nav = document.querySelector('nav');
 
+    let record = window.localStorage.getItem('record');
+    let metaData = window.localStorage.getItem('record_app_metadata');
+
 })();
